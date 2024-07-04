79eeea3d6990a88a8188297443bf5bd71905e0c1
AUSTINMORARA <user.name>
2024-07-04 03:36:25 +0300

    completed

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,10 @@
 # A manifest that install flask
 package {'flask':
   ensure   => '2.1.0',
+<<<<<<< HEAD
   name     => 'flask',
+=======
+  name     => 'flask'
+>>>>>>> db3d08c (completed)
   provider => 'pip3'
 }
