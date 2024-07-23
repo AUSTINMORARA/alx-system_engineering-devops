395c4b38d9827e8ce5f2f02a895145b7bec4c52c
AUSTINMORARA <user.name>
2024-07-23 22:00:01 +0300

    completed

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,13 +1,6 @@
-# server configuration file changes
-
-file_line{ ' no password'
-  ensure => 'present',
-  path   => '/etc/ssh/ssh_config',
-  line   => 'PasswordAuthentication no',
-}
-
-file_line{'~/.ssh/school'
-  ensure => 'present',
-  path   => '/etc/ssh/ssh_config',
-  line   => 'IdentityFile ~/.ssh/school',
+#Reset the configuration passwordauthentication 
+file_line{'/etc/ssh/sshd_config'
+ensure => 'present',
+line   => 'PasswordAuthentication no',
+match  => 'PasswordAuthentication'
 }
