af9b2ca1f85e7e46806a25634b7ad0f7845e73c9
AUSTINMORARA <user.name>
2024-08-23 00:08:48 +0300

    debugging

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,6 @@
 # fix server to server more requests
 
 exec {'modify max open files limit setting':
-  command => 'sed -i "s/15/10000/" /etc/default/nginx && sudo service nginx restart', 
-  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/sbin:/bin:/usr/games:/usr/local/games' 
+  command =>'sed -i "s/15/10000/" /etc/default/nginx && sudo service nginx restart',
+  path    =>'/usr/local/sbin:/usr/local/bin:/usr/sbin:/sbin:/bin:/usr/games:/usr/local/games'
 }
