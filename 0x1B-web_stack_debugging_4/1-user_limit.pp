25abd89c68eb6ddf9e4a2cd9384ab9d5ade6c26b
AUSTINMORARA <user.name>
2024-08-23 00:11:49 +0300

    debugging

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,7 @@
+# changes OS configuration so that it is possible to login as a Holberton user
+# and open file without any error message
+
+exec {'OS security config':
+  command => 'sed -i "s/holberton/foo/" /etc/security/limits.conf',
+  path    => '/usr/bin/env/:/bin/:/usr/bin/:/usr/sbin/'
+}
